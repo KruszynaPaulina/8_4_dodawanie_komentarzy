4fbed88009a281e490f1791b0d12a716198f03b7
Paulina Kruszyna <kruszyna.paulina@gmail.com>
2017-11-24 13:09:25 +0100

    Add

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+var name = prompt('Enter your name'); 
+//alert('Hello, ' + name); 
+console.log('Hello, ' + name); 
+
+/* 
+To jest 
+komentarz 
+blokowy 
+*/
+
+// To jest komentarz liniowy 
